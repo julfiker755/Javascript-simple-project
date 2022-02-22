60eb875dd326cfc2b4596e40618e35dc59c26cca
julfiker755 <julfiker755.bd@gmail.com>
2022-02-22 09:58:22 -0800

    five commit

--- js/app.js
+++ js/app.js
@@ -1,62 +1,42 @@
-// const cbtn1=document.querySelector("#case-push");
-// cbtn1.addEventListener("click",function(){
-//   const caseinput=document.querySelector("#case-number");
-//   const casevalue=caseinput.value;
-//    caseinput.value=parseFloat(casevalue)+1;
-// })
-// const cbtn2=document.querySelector("#case-mins");
-// cbtn2.addEventListener("click",function(){
-// const caseinput=document.querySelector("#case-number");
-// const casevalue=caseinput.value;
-// caseinput.value=parseFloat(casevalue)-1;
-// });
-
-//subtotal
-function gettotalinput(product){
-    const productinput=document.getElementById(product +"-number");
-    const productnumber=parseFloat(productinput.value);
-    return productnumber;
-}
-function calculatetotal(){
- const phonetotal=gettotalinput("phone")*200;
- const casetotal=gettotalinput("case")*60;
- const subtotal=phonetotal+casetotal;
- const tax=subtotal*10/100;
- const total=subtotal+tax;
- //subtotal
- document.querySelector("#subtotal").innerHTML=subtotal;
- document.querySelector("#tax").innerHTML=tax;
- document.querySelector("#total").innerHTML=total;
-}
-
-//phone case function
-function updateproductnumber(product,price,incrysing){
-    const caseinput=document.getElementById(product+"-number");
-    let casenumber=caseinput.value;
-    if(incrysing){
-        casenumber=parseFloat(casenumber)+1;
-    }else if(casenumber>0){
-        casenumber=parseFloat(casenumber)-1;
+function getPin(){
+    const pin=Math.round(Math.random()*10000);
+    const pinString=pin+'';
+    if(pinString.length == 4){
+        return pin;
+    }else{
+        return getPin();
     }
-    caseinput.value=casenumber;
-    const ctotal=document.getElementById(product +"-total");
-    ctotal.innerHTML=casenumber*price;
-    //subtoal//calculate total
-    calculatetotal()
 }
-const cbtn1=document.querySelector("#case-push");
-cbtn1.addEventListener("click",function(){
-    updateproductnumber('case',60,true)
+function geneartepin(){
+   const pin=getPin();
+   document.querySelector("#display-pin").value=pin;
+}
+//key pad
+document.querySelector("#key-pad").addEventListener("click",function(event){
+    const num=event.target.innerText;
+    const calcinput=document.querySelector("#type-number");
+    if(isNaN(num)){
+        if(num =='C'){
+            calcinput.value='';
+        }
+    }else{
+        const previousnum=calcinput.value;
+        const newum=previousnum+num;
+        calcinput.value=newum;
+    }
+
 })
-const cbtn2=document.querySelector("#case-mins");
-cbtn2.addEventListener("click",function(){
-    updateproductnumber('case',60,false)
-});
- const pbtn1=document.querySelector("#phone-push");
- pbtn1.addEventListener("click",function(){
-    updateproductnumber('phone',200,true)
- })
- const pbtn2=document.querySelector("#phone-mins");
- pbtn2.addEventListener("click",function(){
-    updateproductnumber('phone',200,false)
- })
+//varity button
+const successmassage=document.querySelector("#notify-success");
+const errormassage=document.querySelector("#notify-fail");
+function varify(){
+   const pin=document.querySelector("#display-pin").value;
+   const typenumber=document.querySelector("#type-number").value;
+   if(pin==typenumber){
+      successmassage.style.display='block';
+      errormassage.style.display='none'; 
+   }else{
+    errormassage.style.display='block';  
+    successmassage.style.display='none';
+   }
+}
