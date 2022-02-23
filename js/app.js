62117ca4dc2a7704d7bcf004d72dc294f1726e27
julfiker755 <julfiker755.bd@gmail.com>
2022-02-23 00:17:34 -0800

    seven commit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,40 @@
-function getPin(){
-    const pin=Math.round(Math.random()*10000);
-    const pinString=pin+'';
-    if(pinString.length == 4){
-        return pin;
-    }else{
-        return getPin();
-    }
-}
-function geneartepin(){
-   const pin=getPin();
-   document.querySelector("#display-pin").value=pin;
-}
-//key pad
-document.querySelector("#key-pad").addEventListener("click",function(event){
-    const num=event.target.innerText;
-    const calcinput=document.querySelector("#type-number");
-    if(isNaN(num)){
-        if(num =='C'){
-            calcinput.value='';
-        }
-    }else{
-        const previousnum=calcinput.value;
-        const newum=previousnum+num;
-        calcinput.value=newum;
-    }
+//Javascript coundown project
+//Dom
+const days1=document.querySelector("#days");
+const hours1=document.querySelector("#hours");
+const min1=document.querySelector("#min");
+const sec1=document.querySelector("#sec")
 
-})
-//varity button
-const successmassage=document.querySelector("#notify-success");
-const errormassage=document.querySelector("#notify-fail");
-function varify(){
-   const pin=document.querySelector("#display-pin").value;
-   const typenumber=document.querySelector("#type-number").value;
-   if(pin==typenumber){
-      successmassage.style.display='block';
-      errormassage.style.display='none'; 
-   }else{
-    errormassage.style.display='block';  
-    successmassage.style.display='none';
-   }
+//calculate time
+const lastdate="15 March 2022";
+
+function coundown(){
+    const lastnewdate=new Date(lastdate);
+    const update=new Date();
+    const totaltime=(lastnewdate-update)/1000;
+    
+    const days=Math.floor(totaltime/3600/24);
+    const hours=Math.floor(totaltime/3600)%24;
+    const Minutes=Math.floor(totaltime/60)%60;
+    const Seconds=Math.floor(totaltime)%60;
+    days1.innerHTML=days;
+    hours1.innerHTML=hours;
+    min1.innerHTML=Minutes;
+    sec1.innerHTML=Seconds;
+    
 }
+coundown()
+setInterval(coundown,1000)
+
+//Javascript conder project
+//cound dom
+const con = document.querySelector("#cound");
+var count = 1;
+setInterval(() => {
+    // count++;
+    // con.innerHTML = count;
+    if(count<=100){
+        count++;
+        con.innerHTML = count;
+    }
+}, 1000)
