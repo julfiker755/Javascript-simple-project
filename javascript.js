b623d9e01a48d9b9d823068a80c028ea9e972c32
Julfiker Rahaman Jim <97047069+julfiker755@users.noreply.github.com>
2022-03-26 11:30:32 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,3 +15,25 @@ const clearbtn=document.querySelector("#clear-btn");
 clearbtn.addEventListener("click",function(){
     document.querySelector("#type-input").value='';
 })
+//How to Extra code 
+// function calculation(num){
+// const outputscreen=document.querySelector("#inputvalue");
+//  outputscreen.value += num;
+// }
+
+// const equalbtn=document.querySelector("#equal");
+// equalbtn.addEventListener("click",function(){
+//   const outputscreen=document.querySelector("#inputvalue").value;
+//   const total=eval(outputscreen)
+//   document.querySelector("#inputvalue").value=total;
+
+// })
+// const clearbtn=document.querySelector("#clearbtn");
+// clearbtn.addEventListener("click",function(){
+//   document.querySelector("#inputvalue").value='';
+// })
+// const deletebtn=document.querySelector("#deletbtn");
+// deletebtn.addEventListener("click",function(){
+//   const outputscreen=document.querySelector("#inputvalue");
+//   outputscreen.value=outputscreen.value.slice(0,-1)
+// })
